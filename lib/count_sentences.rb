c5c9805a9dbf30a95303d30992aea92a1144543e
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-08-25 21:26:55 +0300

    commit tree df6d332414b2afd89109ee2490b680d3c2a48d0c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.chars[-1] == "." 
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.chars[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.chars[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    if self.chars[-1] == "." && self.chars.count(".") == 1
+      return 1
+    elsif self.chars.include?(".")
+      return self.chars.count(".") + 1
+    else
+      return 0
+    end
   end
 end
